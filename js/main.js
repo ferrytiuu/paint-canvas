c97d6657cd7ad721204e35865e4578e03c575b95
fesolo <fesolo2122daw2@protonmail.com>
2021-11-02 10:24:27 +0100

    02/11/2021

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 window.addEventListener('load', dibujo_canvas);
 
-function dibujo_canvas(){
+let dibujo = false;
+
+function dibujo_canvas() {
+
+
+
+    document.getElementById('icono_pincel').addEventListener('click', function (e) {
+
+        let estado = this.getAttribute("pulsado");
+
+        if (estado === 'true') {
+            this.setAttribute("pulsado", "false");
+            document.getElementById('editor_linea').style.visibility = "hidden";
+            this.src = "imatges/paintbrush.png";
+            console.log("se desactiva");
+            dibujo = false;
+           
+        }
+        else {
+            this.setAttribute("pulsado", "true");
+            document.getElementById('editor_linea').style.visibility = "visible";
+            this.src = "imatges/clicked_paintbrush.png";
+            console.log("Se hace clickS");
+            dibujo = true;
+        }
+
+    });
+
     let isDrawing = false;
     let x = 0;
     let y = 0;
@@ -8,15 +35,18 @@ function dibujo_canvas(){
     const myPics = document.getElementById('area');
     const context = myPics.getContext('2d');
 
-    
+
 
     // event.offsetX, event.offsetY gives the (x,y) offset from the edge of the canvas.
 
     // Add the event listeners for mousedown, mousemove, and mouseup
     myPics.addEventListener('mousedown', e => {
-        x = e.offsetX;
-        y = e.offsetY;
-        isDrawing = true;
+        if(dibujo === true){
+            x = e.offsetX;
+            y = e.offsetY;
+            isDrawing = true;
+        }
+
     });
 
     myPics.addEventListener('mousemove', e => {
@@ -37,12 +67,23 @@ function dibujo_canvas(){
     });
 
     function drawLine(context, x1, y1, x2, y2) {
+        let colorOpacitat = "rgba({},1)"
         context.beginPath();
-        context.strokeStyle = 'black';
-        context.lineWidth = 4;
+        context.strokeStyle = (document.getElementById('selector_color').value),(document.getElementById('opacitat').value);
+        console.log((document.getElementById('selector_color').value),(document.getElementById('opacitat').value));
+        //context.setLineDash([1,1]);
+        context.lineWidth = document.getElementById('grosor_linea_dif').value;
         context.moveTo(x1, y1);
         context.lineTo(x2, y2);
         context.stroke();
         context.closePath();
     }
+
+
+
+
+
+
+
+
 }
